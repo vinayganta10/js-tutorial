57dc79c6ad6333cbf9d349fcd42168dcf40bfd21
vinay ganta <aggvinayganta10@gmail.com>
2023-12-19 21:21:57 +0530

    some changes

--- except.js
+++ except.js
@@ -1,4 +1,4 @@
-const numbers = [1,2,3,4];
+const numbers = [1,2,3,4,5];
 
 const output = except(numbers,[1]);
 
--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 let person = [{name: "vinay",age : 18},{name:"usha",age:16},{name:"rushil",age:18}];
 for(let i of person){
-    
+    console.log(i.name);
+    console.log(i.age)
 }
 
     
